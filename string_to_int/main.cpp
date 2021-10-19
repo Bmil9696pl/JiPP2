4d655105d85c783ec54a5d8c6a3809ef548302b4
bmil96 <bmil9696@gmail.com>
2021-10-19 18:13:35 +0200

    zadania z lab 2

--- string_to_int/main.cpp
+++ string_to_int/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 using namespace std;
+#include <string>
+#include <stdexcept>
 
-
+int konwersja(string str){
+    int x;
+    try {
+        x = stoi(str, nullptr, 10);
+    }
+    catch(std::invalid_argument& e){
+        cerr << e.what() << endl;
+        return 0;
+    }
+    catch(std::out_of_range& e){
+        cerr << e.what() << endl;
+        return 0;
+    }
+    return x;
+}
 
 int main(){
-    
+    string str;
+    int wynik;
+    cout << "wpisz ciag znakow" << endl;
+    cin >> str;
+    wynik = konwersja(str);
+    return wynik;
 }
