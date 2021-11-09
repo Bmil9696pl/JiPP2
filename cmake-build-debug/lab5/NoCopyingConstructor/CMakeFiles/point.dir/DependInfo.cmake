429d61c38aa295a70c41d52aa8ce593ee0061fc7
Bmil9696pl <84275559+Bmil9696pl@users.noreply.github.com>
2021-11-09 17:34:35 +0100

    lab5

---
+++ cmake-build-debug/lab5/NoCopyingConstructor/CMakeFiles/point.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/bartek-tominek/CLionProjects/JiPP2-Bart-omiej-Milecki/lab5/NoCopyingConstructor/src/Point.cpp" "/cygdrive/c/Users/bartek-tominek/CLionProjects/JiPP2-Bart-omiej-Milecki/cmake-build-debug/lab5/NoCopyingConstructor/CMakeFiles/point.dir/src/Point.cpp.o"
+  "/cygdrive/c/Users/bartek-tominek/CLionProjects/JiPP2-Bart-omiej-Milecki/lab5/NoCopyingConstructor/src/main.cpp" "/cygdrive/c/Users/bartek-tominek/CLionProjects/JiPP2-Bart-omiej-Milecki/cmake-build-debug/lab5/NoCopyingConstructor/CMakeFiles/point.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lab5/NoCopyingConstructor/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/lab5/samochod/CMakeFiles/samochod.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/samochod.dir/src/main.cpp.o"
+  "CMakeFiles/samochod.dir/src/samochod.cpp.o"
+  "libsamochod.dll.a"
+  "samochod.exe"
+  "samochod.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/samochod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
